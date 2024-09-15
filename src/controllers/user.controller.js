2eaaf93a6a9ba287ce0aaf3e4f6c079f81086c65
sourya-6 <saisourya678@gmail.com>
2024-09-15 19:25:06 +0530

    access and refresh

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,8 +3,21 @@ import {ApiError} from "../utils/apiError.js"
 import {User} from "../models/user.model.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/apiResponse.js"
-
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken"
+import { ReturnDocument } from "mongodb"
 const registerUser=asyncHandler(async(req,res)=>{
+    // get user details from frontend
+    // validation - not empty
+    // check if user already exists: username, email
+    // check for images, check for avatar
+    // upload them to cloudinary, avatar
+    // create user object - create entry in db
+    // remove password and refresh token field from response
+    // check for user creation
+    // return res
+
+
     // res.status(200).json({
     //     message:"ok"
     // })
@@ -23,15 +36,15 @@ const registerUser=asyncHandler(async(req,res)=>{
    
 
     const existedUser= await User.findOne({//checks whether the user is present or not in the database
-        $or:[{username},{password}]
+        $or:[{username},{email}]
     })
+    
     if(existedUser){
         throw new ApiError(409,"user name already existed try with other one")
     }
+   
 
-    console.log("fullName:",fullName)
     
-    console.log("username:",username)
     const avatarlocalPath=req.files?.avatar[0]?.path
     //const coverImageLocalPath = req.files?.coverImage[0]?.path;
     console.log(avatarlocalPath)
@@ -77,5 +90,98 @@ const registerUser=asyncHandler(async(req,res)=>{
     )
 })
 
+const generateAccessAndRefreshTokens=async(userId)=>{
+    try {
+        const user=await User.findById(userId)
+        const accessToken=user.generateAccessToken()
+        const refreshToken=user.generateRefreshToken()
+        user.refreshToken=refreshToken
+        await user.save({ validateBeforeSave:false })
 
-export {registerUser}
+        return{accessToken,refreshToken}
+
+    } catch (error) {
+        throw new ApiError(500,"Something went wrong while generating the access and refresh tokens")
+    }
+}
+
+const loginUser=asyncHandler(async(req,res)=>{
+    //req body-->data
+    //username or email
+    //find the user
+    //check for the password
+    //access and refresh token generated
+    //send cookie
+    const {username,email,password}=req.body
+
+    if(!username||!email){
+        throw new ApiError(404,"username or email required")
+    }
+
+    const user=await User.findOne({
+        $or:[{username},{email}]
+    })
+
+
+    if(!user){
+        throw new ApiError(404,"User doesn't exist")
+    }
+    
+
+    //password check
+    const isPasswordValid=await user.isPasswordCorrect(password)
+
+    if(!isPasswordValid){
+        throw new ApiError(401,"Password is invalid")
+    }
+
+    const {accessToken,refreshToken}=await generateAccessAndRefreshTokens(user._id)
+
+    const loggedinUser=await User.findById(user._id).select("-password -refreshToken")
+
+    const options={
+        httpOnly:true,
+        secure:true
+    }
+
+    return res
+    .status(200).
+    cookie("accessToken",accessToken)
+    .cookie("refreshToken",refreshToken)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                user: loggedinUser,accessToken,refreshToken
+            },
+            "User logged in successfully"
+        )
+    )
+
+})
+
+const logoutUser=asyncHandler(async(req,res)=>{
+    User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set:{
+                refreshToken:undefined
+            }
+            
+        },
+        {
+            new:true
+        }
+    
+    )
+    const options={
+        httpOnly:true,
+        secure:true
+    }
+    return res
+    .status(200)
+    .clearCookie("accessToken",options)
+    .clearCookie("refreshToken",options)
+    .json(new ApiResponse(200,{},"User logged Out"))
+})
+export {registerUser,loginUser,logoutUser}
